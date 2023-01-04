37dcc9aa03a5424e11955ae49ca12aec0279949e
9yubean2 <dlrbqls3024@swu.ac.kr>
2023-01-04 22:42:18 +0900

    Design: Footer Component 구성

---
+++ src/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#ffffff] p-4 sm:px-6 md:px-6 lg:px-10 xl:px-10 2xl:px-10">
+            
+    <div className="sm:flex-col sm:items-center ">
+        {/* <span className="self-center text-[24px] font-semibold whitespace-nowrap">✅ Todos</span> */}
+        <p className="font-semibold text-[18px] cursor-default">원티드 프리온보딩 챌린지 프론트엔드 코스</p>
+        <p className="font-medium text-[16px] cursor-default">1월 챌린지, CRUD w React Query</p>
+
+    </div>
+
+    <article className="flex items-start space-x-6 py-3">
+      
+      <img src="" alt="" width="60" height="60" className="flex-none rounded-md bg-slate-100" />
+      
+      <div className="min-w-0 relative flex-auto">
+        <h2 className="font-semibold text-slate-900 truncate pr-20">이규빈</h2>
+        <dl className="mt-2 flex flex-wrap text-sm leading-6 font-medium">
+          <div>
+            <dt className="sr-only">SkillSet</dt>
+            <dd className="mr-2 px-1.5 ring-1 ring-slate-200 rounded"># Frontend</dd>
+          </div>
+          <div>
+            <dt className="sr-only">SkillSet</dt>
+            <dd className="mr-2 px-1.5 ring-1 ring-slate-200 rounded"># CloudEngineer</dd>
+          </div>
+          
+          
+          
+          <div className="flex-none w-full mt-2 font-normal">
+            <dt className="sr-only">Cast</dt>
+            <dd className="text-slate-400">movie.runtime</dd>
+          </div>
+        </dl>
+      </div>
+    </article>
+
+
+    <hr className="mb-3 border-gray-200 lg:mb-3"/>
+    <span className="block text-sm text-gray-500 dark:text-gray-400 text-[18px] font-suit font-normal cursor-default">© 2023 Todos All Rights Reserved</span>
+</footer>
+  )
+}
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function Main() {
 
     return (
-        <div className="min-h-screen flex flex-col w-full h-full">
+        <div className="h-screen flex flex-col w-full bg-slate-500">
             <Header/>
-
             
-        </div>
+            <div className="grid grid-cols-3 flex-1">
+                <div className="min-h-full flex flex-col w-full h-full bg-red-200">
+                    <ul>
+                        <li className="flex items-center justify-between py-3">
+                            <div>
+                                <input type="checkbox" />
+                                <span className="ml-5 align-middle">할 일 1</span>
+                            </div>                                
+                            <div>
+                                <button className="align-middle py-1 px-3 text-[#6E9DC9] font-semibold text-[14px] cursor-pointer" >수정</button>
+                                <button className="align-middle py-1 px-3 rounded-[9px] bg-[#EC7A7A] text-[#ffffff] font-semibold text-[14px] cursor-pointer" >삭제</button>
+                            </div>                            
+                        </li>
+                    </ul>
+                </div>
 
+                <div className="col-span-2 bg-indigo-500">
+
+                </div>
+            </div>
+            
+            <Footer/>
+        </div>
     )
 }
